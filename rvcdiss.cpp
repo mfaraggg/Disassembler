b1e2f95c66a6cb31d1b9a159cac3ea01d453a0af
mfaraggg <64089004+mfaraggg@users.noreply.github.com>
2021-06-28 15:39:08 +0200

    Update rvcdiss.cpp

--- rvcdiss.cpp
+++ rvcdiss.cpp
@@ -88,6 +88,49 @@ void instDecExec(unsigned int instWord)
 
 }
 
+void R_Type(unsigned int instWord) 
+{
+	unsigned int rd, rs1, rs2, funct3, funct7 = 0, opcode;
+	//unsigned int I_imm, S_imm, B_imm, U_imm, J_imm;
+	unsigned int address;
+
+	unsigned int instPC = pc - 4;
+
+	opcode = instWord & 0x0000007F;
+	rd = (instWord >> 7) & 0x0000001F;
+	funct3 = (instWord >> 12) & 0x00000007;
+	rs1 = (instWord >> 15) & 0x0000001F;
+	rs2 = (instWord >> 20) & 0x0000001F;
+
+	printPrefix(instPC, instWord);
+
+	switch (funct3) {
+
+	}
+}
+
+void I_Type(unsigned int instWord)
+{
+	unsigned int rd, rs1, imm, funct3, funct7 = 0, opcode;
+	//unsigned int I_imm, S_imm, B_imm, U_imm, J_imm;
+	unsigned int address;
+
+	unsigned int instPC = pc - 4;
+
+	opcode = instWord & 0x0000007F;
+	rd = (instWord >> 7) & 0x0000001F;
+	funct3 = (instWord >> 12) & 0x00000007;
+	rs1 = (instWord >> 15) & 0x0000001F;
+	imm = (instWord >> 20) & 0x00000FFF;
+
+	printPrefix(instPC, instWord);
+
+	switch (funct3) {
+
+
+	}
+}
+
 int main(int argc, char *argv[]){
 
 	unsigned int instWord=0;
