b4eb0dbde69104d4b4809f3ce424e5047c615445
mfaraggg <64089004+mfaraggg@users.noreply.github.com>
2021-06-28 16:56:13 +0200

    Update rvcdiss.cpp

--- rvcdiss.cpp
+++ rvcdiss.cpp
@@ -86,10 +86,9 @@ void instDecExec(unsigned int instWord)
 
 }
 
-void R_Type(unsigned int instWord) 
+void R_Type(unsigned int instWord)
 {
 	unsigned int rd, rs1, rs2, funct3, funct7 = 0, opcode;
-	//unsigned int I_imm, S_imm, B_imm, U_imm, J_imm;
 	unsigned int address;
 
 	unsigned int instPC = pc - 4;
@@ -99,21 +98,52 @@ void R_Type(unsigned int instWord)
 	funct3 = (instWord >> 12) & 0x00000007;
 	rs1 = (instWord >> 15) & 0x0000001F;
 	rs2 = (instWord >> 20) & 0x0000001F;
+	funct7 = (instWord >> 25) & 0x0000007F;
 
 	printPrefix(instPC, instWord);
 
-	if(opcode == 0x33){        // R Instructions
-        switch(funct3){
-            case 0: if(funct7 == 32) {
-                                cout << "\tSUB\tx" << rd << ", x" << rs1 << ", x" << rs2 << "\n";
-                            }
-                            else {
-                                cout << "\tADD\tx" << rd << ", x" << rs1 << ", x" << rs2 << "\n";
-                            }
-                            break;
-            default:
-                            cout << "\tUnkown R Instruction \n";
-        }
+	switch (funct3) {
+	case 0: 
+		if (funct7 == 0)
+			cout << "\tADD\tx" << rd << ", x" << rs1 << ", x" << rs2 << "\n";
+		else
+			cout << "\tSUB\tx" << rd << ", x" << rs1 << ", x" << rs2 << "\n";
+		break;
+
+	case 1:
+		cout << "\tSLL\tx" << rd << ", x" << rs1 << ", x" << rs2 << "\n";
+		break;
+
+	case 2:
+		cout << "\tSLT\tx" << rd << ", x" << rs1 << ", x" << rs2 << "\n";
+		break;
+
+	case 3:
+		cout << "\tSLTU\tx" << rd << ", x" << rs1 << ", x" << rs2 << "\n";
+		break;
+
+	case 4:
+		cout << "\tXOR\tx" << rd << ", x" << rs1 << ", x" << rs2 << "\n";
+		break;
+
+	case 5:
+		if (funct7 == 0)
+			cout << "\tSRL\tx" << rd << ", x" << rs1 << ", x" << rs2 << "\n";
+		else
+			cout << "\tSRA\tx" << rd << ", x" << rs1 << ", x" << rs2 << "\n";
+		break;
+
+	case 6:
+		cout << "\tOR\tx" << rd << ", x" << rs1 << ", x" << rs2 << "\n";
+		break;
+
+	case 7:
+		cout << "\tAND\tx" << rd << ", x" << rs1 << ", x" << rs2 << "\n";
+		break;
+
+	default:
+		cout << "\tUnkown R Instruction \n";
+	}
 }
 
 void I_Type(unsigned int instWord)
