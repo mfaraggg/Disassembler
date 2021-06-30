3060d98355f57813edcbcb8c5e02c7b859ebf222
Nagham Mekky <nagham.mekky@aucegypt.edu>
2021-06-30 17:16:29 +0200

    Fixing immediate output

--- rvcdiss.cpp
+++ rvcdiss.cpp
@@ -115,7 +115,7 @@ void I_Type(unsigned int instWord)
 	rd = (instWord >> 7) & 0x0000001F;
 	funct3 = (instWord >> 12) & 0x00000007;
 	rs1 = (instWord >> 15) & 0x0000001F;
-	imm = ((instWord >> 20) & 0x7FF) | (((instWord >> 31) ? 0x800 : 0x0));
+	imm = ((instWord >> 20) & 0x7FF) | (((instWord >> 31) ? 0xFFFFF800 : 0x0));
 
 	printPrefix(instPC, instWord);
 
@@ -208,7 +208,8 @@ void S_Type(unsigned int instWord)
 	imm2 = (instWord >> 25) & 0x0000003F;
 	imm = imm2;
 	imm = (imm2 << 5) | imm1;
-	//imm = (imm) | (((instWord >> 31) ? 0x40 : 0x0));
+	imm = (imm) | (((instWord >> 31) ? 0xFFFFF800 : 0x0));
+
 	printPrefix(instPC, instWord);
 
 	switch (funct3) {
